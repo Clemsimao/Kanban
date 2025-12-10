1e9f01f2a7baa748f3e6c5c3e49d5e7289d8f3dc
Clément Simao <71353995+Clemsimao@users.noreply.github.com>
2025-12-10 09:30:35 +0100

    feat: Initialize client project with Vite and refine Docker Compose for API and database setup.

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,5 +1,8 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  envDir: "." // on remonte d'un dossier pour y trouver le fichier .env
+  envDir: ".", // on remonte d'un dossier pour y trouver le fichier .env
+  define: {
+    'import.meta.env.VITE_API_BASE_URL': JSON.stringify('http://localhost:3000/api')
+  }
 });
